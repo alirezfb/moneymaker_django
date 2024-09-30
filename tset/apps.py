3ecf34683ab04f110a99e22f73258c026cdab1de
alirezfb <alirezfb@protonmail.com>
2024-09-30 17:35:16 +0330

    latest

---
+++ tset/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TsetConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tset'
--- tset/urls.py
+++ tset/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path:whole urls /
+    # slug:hypen-and_underscore_stuff
+    # UUID:Universally unique identifier
+
     path('', views.home, name="home"),
+    path('<int:year>/<str:month>/', views.home, name="home"),
 ]
